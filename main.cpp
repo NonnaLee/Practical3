6ecfeb33a991d5963924ee3487076f78d003fcc5
Jia-Ping Lee <a1812034@student.adelaide.edu.au>
2022-03-20 15:26:10 +1030

    i8

---
+++ main.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include "Referee.h"
+#include "Human.h"
+
+using namespace std;
+
+int main()
+{
+	Human humanPlayer = Human();
+	cout << humanPlayer.makeMove();
+	return 0;
+
+}
+
