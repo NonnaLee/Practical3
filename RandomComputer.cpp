9ef49021c0cc5c9573a2ba5cbe67fc6d1d939117
Jia-Ping Lee <a1812034@student.adelaide.edu.au>
2022-03-21 15:45:11 +1030

    Fix random computer file

--- RandomComputer.cpp
+++ RandomComputer.cpp
@@ -7,7 +7,7 @@ RandomComputer::RandomComputer()
 
 char RandomComputer::makeMove() {
     char moves[3] = { 'R','S','P' };
-    char randomMove = rand() % 3;
+    int randomMove = rand() % 3;
 
     return moves[randomMove]; ;
 };
